343f163c145e13e43cdce3840682d85d2d892a3f
Arnol Guevara <arnolpacman@gmail.com>
2024-08-07 12:33:23 -0400

    updated

--- src/root/models/entities/speed-reducer.entity.ts
+++ src/root/models/entities/speed-reducer.entity.ts
@@ -1,3 +1,4 @@
+import { Point } from 'geojson';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({
@@ -8,6 +9,14 @@ export class SpeedReducerEntity {
     name: 'id',
   })
   id?: number;
+
+  @Column({
+    name: 'geom',
+    type: 'geometry',
+    spatialFeatureType: 'Point',
+    srid: 4326,
+  })
+  geom?: Point;
 
   @Column({
     type: 'varchar',
